7c0693e50297729a68be4f086f3630c9e64be3d0
Roobios <84737929+Roobios@users.noreply.github.com>
2021-10-05 15:17:06 +0200

    Add files via upload

---
+++ exo_17.rb
@@ -0,0 +1,23 @@
+print "Combien d'étages veux-tu pour ta pyramide?"
+
+
+nb = gets.chomp.to_i
+
+until nb.between? 1, 25 do
+    print "Veuillez choisir un nombre entre 1 et 25: "
+    nb = gets.chomp.to_i
+end
+
+puts "Voici la pyramide :"
+
+nb.times do |i|
+    print " " * (nb-i-1)
+    puts "#" * i + "#" + "#" * i
+end
+
+
+    #
+   ###
+  #####
+ #######
+#########
---
+++ pyramide.rb
@@ -0,0 +1,15 @@
+print "Combien d'étages veux-tu pour ta pyramide?"
+
+nb = gets.chomp.to_i
+
+until nb.between? 1, 25 do
+    print "Veuillez choisir un nombre entre 1 et 25: "
+    nb = gets.chomp.to_i
+end
+
+puts "Voici la pyramide :"
+
+nb.times do |i|
+    print " " * (nb-i-1)
+    puts "#" * (i + 1)
+end
